a77ae9c633840868ac6e0be8c10b3d59b411cf51
fcamilo23 <facundo.camilo@estudiantes.utec.edu.uy>
2022-05-06 20:25:02 -0300

    QWE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes/clientes.component';
+import { MenuComponent } from './menu/menu/menu.component';
+import { ProductosComponent } from './productos/productos/productos.component';
+import { VentasComponent } from './ventas/ventas/ventas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'ventas', component: VentasComponent },
+  { path: 'productos', component: ProductosComponent }
+  { path: '', component: MenuComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes/clientes.component';
+import { ProductosComponent } from './productos/productos/productos.component';
+import { VentasComponent } from './ventas/ventas/ventas.component';
+import { MenuComponent } from './menu/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    ProductosComponent,
+    VentasComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
