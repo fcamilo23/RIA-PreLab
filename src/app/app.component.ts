7e9585e2527b400b39f772bf9631f909b1847c0a
fcamilo23 <facundo.camilo@estudiantes.utec.edu.uy>
2022-05-06 03:28:47 -0300

    Clientes (alta y listado)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RIA-PreLab';
+  title:string = 'RIA-PreLab';
+
+  clientes = [
+    {'documento':'11111111', 'nombre':'Facundo', 'apellido':'Camilo', 'fechaNac':'13/06/1999', 'direccion':'Rivera 419', 'telefono':'099044004' },
+  ];
+
+  model:any = {};
+  
+  addCliente():void{
+    this.clientes.push(this.model)
+  }
+
+  deleteCliente():void{
+
+  }
+
+  editCliente():void{
+
+  }
+
+  updateCliente():void{
+
+  }
 }
